20c1d215153afc9a2a7e02649223b2f9052b9dd0
Janujan <janujan.selva@gmail.com>
2021-05-23 18:41:24 -0400

    pivoting to twitter

--- app.py
+++ app.py
@@ -1,30 +1,21 @@
-from nba_api.stats.endpoints import commonplayerinfo
-from nba_api.stats.endpoints import commonallplayers
 from flask import Flask
+from flask import jsonify
 import pandas
 import requests
+import twitter
+import json
 
 app = Flask(__name__)
 
+@app.route("/metrics/<string:username>")
+def metric_lookup(username):
+    token = twitter.retrieve_token()
+    return jsonify(twitter.get_metrics_for_tweets(username, token))
 
-
-@app.route("/playerSearch/<string:playername>")
-def player(playername):
-        # populate player dictionary to map ids to player names
-    players = commonallplayers.CommonAllPlayers(is_only_current_season=1)
-    player_list = players.data_sets[0].data['data']
-    player_dict = {}
-
-    for player in player_list:
-        # sixth element contains name in lower case, no puncation
-        player_dict[player[6]] = player[0]
-        
-    convert_query = str.lower('_'.join(playername.split(' ')))
-
-    if convert_query in player_dict.keys():
-        player_info = commonplayerinfo.CommonPlayerInfo(player_id=player_dict[convert_query])
-        return player_info.get_data_frames()[0].to_dict(orient='records')[0]
-    return "Player not found :("
+@app.route("/tweets/<string:username>")
+def tweet_lookup(username):
+    token=twitter.retrieve_token()
+    return jsonify(twitter.get_user_tweets(username, token))
 
 @app.route("/")
 def hello():
---
+++ twitter.py
@@ -0,0 +1,27 @@
+import requests
+import os
+from requests.auth import HTTPBasicAuth
+
+api_key = os.environ['API_KEY'] 
+api_secret = os.environ['API_SECRET'] 
+auth = HTTPBasicAuth(api_key, api_secret)
+
+def retrieve_token():
+    response = requests.post('https://api.twitter.com/oauth2/token', params={'grant_type': 'client_credentials'}, auth=auth)
+    response.raise_for_status()
+    return response.json()['access_token']
+
+def get_user_id(username, token):
+    response = requests.get(f'https://api.twitter.com/2/users/by/username/{username}', headers = {"Authorization": f"Bearer {token}"})
+    return response.json()['data']['id']
+
+def get_user_tweets(username, token):
+    user_id = get_user_id(username, token)
+    response = requests.get(f'https://api.twitter.com/2/users/{user_id}/tweets', headers = {"Authorization": f"Bearer {token}"})
+    return response.json()['data']
+
+def get_metrics_for_tweets(username, token):
+    tweets = get_user_tweets(username, token)
+    tweet_ids = [tweet['id'] for tweet in tweets]
+    response = requests.get(f'https://api.twitter.com/2/tweets', params={'ids':','.join(tweet_ids), 'tweet.fields':['public_metrics']} ,headers = {"Authorization": f"Bearer {token}"})
+    return response.json()['data']
