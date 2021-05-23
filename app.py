7a5d719cdbdf53577bc66c2f3cb1efedef3ce1b0
Janujan <janujan.selva@gmail.com>
2021-05-22 22:50:36 -0400

    moving location of api calls to debug timeout issue

--- app.py
+++ app.py
@@ -5,17 +5,19 @@ import pandas
 
 app = Flask(__name__)
 
-# populate player dictionary to map ids to player names
-players = commonallplayers.CommonAllPlayers(is_only_current_season=1)
-player_list = players.data_sets[0].data['data']
-player_dict = {}
 
-for player in player_list:
-    # sixth element contains name in lower case, no puncation
-    player_dict[player[6]] = player[0]
 
 @app.route("/playerSearch/<string:playername>")
 def player(playername):
+        # populate player dictionary to map ids to player names
+    players = commonallplayers.CommonAllPlayers(is_only_current_season=1)
+    player_list = players.data_sets[0].data['data']
+    player_dict = {}
+
+    for player in player_list:
+        # sixth element contains name in lower case, no puncation
+        player_dict[player[6]] = player[0]
+        
     convert_query = str.lower('_'.join(playername.split(' ')))
 
     if convert_query in player_dict.keys():
