e6b2d25117604508861dcc809fd2e7de078f32ac
Janujan <janujan.selva@gmail.com>
2021-05-24 12:31:45 -0400

    changing caching mechanism to have shorter timeout (2 mins)

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import jsonify
 from flask import g #holds information on the request context
+from flask_caching import Cache
 import pandas
 import requests
 import twitter
@@ -8,6 +9,16 @@ import time
 import json
 
 app = Flask(__name__)
+
+cache_config = {
+    "DEBUG": True,          # some Flask specific configs
+    "CACHE_TYPE": "SimpleCache",  # Flask-Caching related configs
+    "CACHE_DEFAULT_TIMEOUT": 300
+}
+
+app.config.from_mapping(cache_config)
+
+cache = Cache(app)
 
 @app.before_request
 def before_request():
@@ -19,11 +30,13 @@ def after_request(exception):
     print(f'time elapsed for request : {end}')
 
 @app.route("/metrics/<string:username>")
+@cache.memoize(timeout=120)
 def metric_lookup(username):
     token = twitter.retrieve_token()
     return jsonify(twitter.get_metrics_for_tweets(username, token))
 
 @app.route("/tweets/<string:username>")
+@cache.memoize(timeout=120)
 def tweet_lookup(username):
     token=twitter.retrieve_token()
     return jsonify(twitter.get_user_tweets(username, token))
--- twitter.py
+++ twitter.py
@@ -1,7 +1,6 @@
 import requests
 import os
 from requests.auth import HTTPBasicAuth
-from functools import lru_cache
 
 api_key = os.environ['API_KEY'] 
 api_secret = os.environ['API_SECRET'] 
@@ -16,13 +15,11 @@ def get_user_id(username, token):
     response = requests.get(f'https://api.twitter.com/2/users/by/username/{username}', headers = {"Authorization": f"Bearer {token}"})
     return response.json()['data']['id']
 
-@lru_cache
 def get_user_tweets(username, token):
     user_id = get_user_id(username, token)
     response = requests.get(f'https://api.twitter.com/2/users/{user_id}/tweets', headers = {"Authorization": f"Bearer {token}"})
     return response.json()['data']
 
-@lru_cache
 def get_metrics_for_tweets(username, token):
     tweets = get_user_tweets(username, token)
     tweet_ids = [tweet['id'] for tweet in tweets]
